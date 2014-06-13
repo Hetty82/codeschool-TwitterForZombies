412768c8ac2866f55c372bf07c5ce3b3a69a7131
Hetty de Vries <devries@hetty82.nl>
2014-06-13 09:59:00 +0200

    "Initial commit"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterForZombies::Application.config.secret_token = 'd9913a495cc805a212be6c302551e9e0f8c624992a54a546b4e5ef26fa27c0112c69d26401fd644b3757db7daf85a7a17d97f878a1341b2f7d8a16dae4f1a698'
